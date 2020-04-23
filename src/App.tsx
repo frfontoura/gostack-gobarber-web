98ad47b7d3ca51263588a1d5989f452b6af0f992
frfontoura <frfontoura@gmail.com>
2020-04-22 21:40:37 -0300

    Criação da pagina de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
+import SignIn from './pages/SignIn';
+
 const App: React.FC = () => {
-  return <div className="App">Hello World</div>;
+  return (
+    <>
+      <SignIn />
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
